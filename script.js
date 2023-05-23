3b2ab199a934801855f1d5cd80ed6afe7350d3a1
igor <117731472+manfan7@users.noreply.github.com>
2023-05-23 19:45:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ $(".select-wrapper").click(function () {
   $(".dropdownoption").toggleClass("dropdownoptionop");
 });
 $(".item__li-select-wrapper").click(function () {
-  $(this).toggleClass("item__li-open");
+  $(this).toggleClass("open1");
   $(".item__li-dropdownoption__li").toggleClass(
     "item__li-dropdownoptionop__li"
   );
 });
 $(".tarif-enterpice-ch").click(function () {
   $(".tarif-enterpice-ch1").toggleClass("opening");
+});
+$(".item__li-select-wrapper1").click(function () {
+  $(this).toggleClass("open1");
+  $(".item__li-dropdownoption__li1").toggleClass(
+    "item__li-dropdownoptionop__li1"
+  );
 });
