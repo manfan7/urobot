755e816a10ea4c5cc3152131d270459200a725fa
igor <117731472+manfan7@users.noreply.github.com>
2023-05-25 19:58:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-$(".wr1").click(function () {
-  $(this).toggleClass("open2");
-  $(".op1").toggleClass("dropdownoptionop");
-});
-$(".wr2").click(function () {
-  $(this).toggleClass("open2");
-  $(".op2").toggleClass("dropdownoptionop2");
-});
-
 $(".item__li-select-wrapper").click(function () {
   $(this).toggleClass("open1");
   $(".item__li-dropdownoption__li").toggleClass(
@@ -22,17 +13,41 @@ $(".item__li-select-wrapper1").click(function () {
     "item__li-dropdownoptionop__li1"
   );
 });
+$(".wr1").click(function () {
+  $(".wr11").toggleClass("open2");
+  $(".op1").toggleClass("dropdownoptionop");
+});
+
+$(".wr2").click(function () {
+  $(".wr22").toggleClass("open2");
+  $(".op2").toggleClass("dropdownoptionop2");
+});
 let option1 = document.querySelectorAll(".option1");
 let option2 = document.querySelectorAll(".option2");
+let li1 = document.querySelector(".li1");
+let li2 = document.querySelector(".li2");
+let li22 = document.querySelector(".li22");
+let arrow = document.querySelector(".arrow_example_li");
 
-document.querySelector(".li1").addEventListener("click", function (event) {
+li1.addEventListener("click", function (event) {
   option1.forEach((item) => item.classList.toggle("optionopen"));
-  event.target.classList.toggle("openli");
+  document.querySelector(".arrow_example_li").classList.toggle("openli");
+  if (li22.classList.contains("openli")) {
+    option2.forEach((item) => item.classList.toggle("optionopen"));
+    document.querySelector(".li22").classList.toggle("openli");
+  }
 });
-document.querySelector(".li2").addEventListener("click", function (event) {
+
+li2.addEventListener("click", function (event) {
   option2.forEach((item) => item.classList.toggle("optionopen"));
-  event.target.classList.toggle("openli");
+  document.querySelector(".li22").classList.toggle("openli");
+  if (arrow.classList.contains("openli")) {
+    option1.forEach((item) => item.classList.toggle("optionopen"));
+    document.querySelector(".arrow_example_li").classList.toggle("openli");
+  }
 });
+let optionopen = document.querySelectorAll(".optionopen");
+let optionopenli = document.querySelectorAll(".openli");
 let skewtext1 = document.querySelector(".skewtext1");
 let skewtext2 = document.querySelector(".skewtext2");
 let skewtextsec = document.querySelectorAll(".skewtext-sec");
@@ -61,10 +76,15 @@ let dolzs44 = document.querySelector("#dolzs44");
 let itogSum;
 let dropdownoption1 = document.querySelectorAll(".op1");
 let dropdownoption2 = document.querySelectorAll(".op2");
+let dropdownskew = document.querySelector(".dropdown-skewblock");
+let dropdownskewsk2 = document.querySelector(".sk2");
+let dropdownskewsec = document.querySelectorAll(".dropdown-skewblock-sec");
 dropdownoption1.forEach((item) => {
   item.addEventListener("click", function (event) {
     let saleText = event.target.innerHTML;
     saleSum1 = +event.target.dataset.sale;
+    dropdownskew.style.display = "flex";
+    dropdownskewsec.forEach((item) => (item.style.display = "flex"));
     yearArticle1.innerHTML = saleText;
     skewtext1.innerHTML = saleSum1;
     skewtextsec.forEach((item) => (item.innerHTML = saleSum1));
@@ -74,6 +94,10 @@ dropdownoption1.forEach((item) => {
     dolzs3.innerHTML = datadolz3;
     dolzs33.innerHTML = datadolz3 - (datadolz3 * saleSum1) / 100;
 
+    if (saleSum1 == 0) {
+      dropdownskew.style.display = "none";
+      dropdownskewsec.forEach((item) => (item.style.display = "none"));
+    }
     // const content = element.innerHTML;
   });
 });
@@ -81,6 +105,7 @@ dropdownoption2.forEach((item) => {
   item.addEventListener("click", function (event) {
     let saleText = event.target.innerHTML;
     saleSum2 = +event.target.dataset.sale;
+    dropdownskewsk2.style.display = "flex";
     yearArticle2.innerHTML = saleText;
     skewtext2.innerHTML = saleSum2;
     dolzs4.innerHTML = datadolz4;
@@ -93,6 +118,9 @@ dropdownoption2.forEach((item) => {
     } else {
       dolzs5.innerHTML = "Индивидуально";
     }
+    if (saleSum2 == 0) {
+      dropdownskewsk2.style.display = "none";
+    }
   });
 });
 
@@ -102,6 +130,8 @@ dolzopt1.forEach((item) => {
     let datadolz = +event.target.dataset.dolz;
     dolzs1.innerHTML = datadolz;
     dolzs11.innerHTML = datadolz - (datadolz * saleSum1) / 100;
+    option1.forEach((item) => item.classList.toggle("optionopen"));
+    document.querySelector(".arrow_example_li").classList.toggle("openli");
   });
 });
 dolzopt2.forEach((item) => {
@@ -110,6 +140,8 @@ dolzopt2.forEach((item) => {
     let datadolz2 = +event.target.dataset.dolz2;
     dolzs2.innerHTML = datadolz2;
     dolzs22.innerHTML = datadolz2 - (datadolz2 * saleSum1) / 100;
+    option2.forEach((item) => item.classList.toggle("optionopen"));
+    document.querySelector(".li22").classList.toggle("openli");
   });
 });
 dolzopt3.forEach((item) => {
